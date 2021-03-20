940d8478151f01ffa6e6323d00eef182da892d0c
yllens <syllen@protonmail.com>
2021-03-20 16:32:51 +0100

    added more functions

--- read_corpus.py
+++ read_corpus.py
@@ -2,7 +2,8 @@ import create_corpus as cc
 from collections import defaultdict
 
 
-def read_tweets(single_file=True, users_file=None, tweets_file=None, users_dir=None, tweets_dir=None):
+def read_tweets(single_file=True, users_file=None, tweets_file=None,
+                users_dir=None, tweets_dir=None):
     """ Extract a list of all users from the user masterlist file along with their corresponding tweets
     in the tweet masterlist file
 
@@ -43,26 +44,56 @@ def read_tweets(single_file=True, users_file=None, tweets_file=None, users_dir=N
     return users_tweets
 
 
-def find_num_tweets(tweet_data, num_tweets=1):
+def group_tweets(single_file=True, users_file=None, tweets_file=None,
+                 users_dir=None, tweets_dir=None):
+    if single_file:
+        tweet_data = read_tweets(single_file=True, users_file=users_file, tweets_file=tweets_file)
+    else:
+        tweet_data = read_tweets(single_file=False, users_dir=users_dir, tweets_dir=tweets_dir)
+
+    # Convert list of lists to dict to count the number of tweets per user
+    tweets_dict = defaultdict(list)
+    for user, tweet in tweet_data:
+        tweets_dict[user].append(tweet)
+
+    return tweets_dict
+
+
+def get_num_tweets(single_file=True, num_tweets=1, users_file=None, tweets_file=None,
+                   users_dir=None, tweets_dir=None):
     """ Find users with more than one tweet.
 
+    :param single_file: If True, retrieve data only for single file
+                        If False, retrieve data from multiple files in user/tweet directories
+    :param users_file:  Path to single users masterlist file
+    :param tweets_file: Path to single tweets masterlist file
+    :param users_dir:   Path to directory of users masterlist files
+    :param tweets_dir:  Path to directory of tweets masterlist files
     :param tweet_data:  Input list of lists returned by read_tweets()
     :param num_tweets:  Number of tweets to look for
 
     :return: List of tuples containing users with more than one tweet in tweet_data, along with the number of tweets
     """
-    # Convert list of lists to dict to count the number of tweets per user
-    tweets_dict = defaultdict(list)
-    for k, v in tweet_data:
-        tweets_dict[k].append(v)
+    tweets_dict = group_tweets(single_file=single_file, users_file=users_file, tweets_file=tweets_file,
+                               users_dir=users_dir, tweets_dir=tweets_dir)
 
     # Filter users by specified number of tweets
     users = []
     for item in list(tweets_dict.items()):
         if len(item[1]) > num_tweets:
-            users.append((item[0], len(item)))
+            users.append((item[0], len(item[1])))
 
     # Sort list in descending order by number of tweets
-    users.sort(key=lambda x: x[1])
+    users.sort(key=lambda x: x[1], reverse=True)
 
     return users
+
+
+def get_tweets(tweets_dict, user):
+    """ Retrieve all tweets for a given user.
+
+    :param tweets_dict: Input dictionary of tweets grouped by user
+    :param user:        Target user whose tweets to extract
+    :return:            List of tweets
+    """
+    return tweets_dict.get(user)
